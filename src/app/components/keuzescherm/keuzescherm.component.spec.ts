ee422dc7f193fedfcdcd2085a797af6f55948d82
MelindaDeRoo <melindaderoo@gmail.com>
2021-08-22 21:04:32 +0200

    update 11

--- src/app/components/keuzescherm/keuzescherm.component.spec.ts
+++ src/app/components/keuzescherm/keuzescherm.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { KeuzeschermComponent } from './keuzescherm.component';
 
@@ -6,7 +6,7 @@ describe('KeuzeschermComponent', () => {
   let component: KeuzeschermComponent;
   let fixture: ComponentFixture<KeuzeschermComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ KeuzeschermComponent ]
     })
